89cce02849ae1b54569fa4c7c35de139c87734e0
Moises Holguin <mholguin@cs.utexas.edu>
2014-11-15 15:46:57 -0600

    Shuffle method for randomly assingning roles

---
+++ Mafia/Mafia/NSMutableArray_Shuffle.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray_Shuffle.h
+//  Mafia
+//
+//  Created by Moises Holguin on 11/15/14.
+//  Copyright (c) 2014 Group13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSMutableArray (Shuffle)
+-(void)shuffle;
+@end
---
+++ Mafia/Mafia/NSMutableArray_Shuffle.m
@@ -0,0 +1,20 @@
+//
+//  NSMutableArray_Shuffle.m
+//  Mafia
+//
+//  Created by Moises Holguin on 11/15/14.
+//  Copyright (c) 2014 Group13. All rights reserved.
+//
+
+#import "NSMutableArray_Shuffle.h"
+
+@implementation NSMutableArray (Shuffle)
+- (void)shuffle {
+    NSUInteger count = [self count];
+    for(NSUInteger i = 0; i < count; ++i) {
+        NSInteger remainingCount = count - i;
+        NSInteger exchangeIndex = i + arc4random_uniform((arc4random_uniform((u_int32_t)remainingCount)));
+        [self exchangeObjectAtIndex:i withObjectAtIndex:exchangeIndex];
+    }
+}
+@end
