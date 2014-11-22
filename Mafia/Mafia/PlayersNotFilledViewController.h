94e1122822435dda0117745fba5b578772d7f433
KevinNguyenn <kevinnguyen@wireless-10-147-165-150.public.utexas.edu>
2014-11-22 14:39:58 -0600

    resolve players not filled VC

---
+++ Mafia/Mafia/PlayersNotFilledViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlayersNotFilledViewController.h
+//  Mafia
+//
+//  Created by Kevin Nguyen on 11/22/14.
+//  Copyright (c) 2014 Group13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayersNotFilledViewController : UIViewController
+
+- (IBAction)goBackToCards:(UIStoryboardSegue *)sender;
+
+@end
